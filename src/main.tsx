d5a71455346d16e8b2687c345d817dc82f19332f
spidunno <70819261+spidunno@users.noreply.github.com>
2024-07-07 10:51:57 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import './index.css';
 import '@fontsource/fira-code';
 import '@fontsource/fira-mono';
 
+const FLAG = '\x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[41m                \x1b[0m          Happy Pride\n\x1b[105m  \x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[33;41m░░░░░░░░░░░░░░\n\x1b[47m  \x1b[105m  \x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[43m            \n\x1b[47m  \x1b[105m  \x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[42m            \x1b[0m          Every month is pride month\n\x1b[105m  \x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[44m              \n\x1b[104m  \x1b[41;30m▒▒\x1b[40m  \x1b[45m                ';
+console.log(FLAG);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
